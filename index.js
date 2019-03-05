088c291f6b06658754be2271a49ebc0ad9c17d22
Jared <jaredbolduc@yahoo.com>
2019-03-04 18:48:18 -0700

    saved

---
+++ index.js
@@ -0,0 +1,43 @@
+//Nouns adjective and verbs
+// basic data types
+'abc' "abc" 'abc' // strings
+10 //numbers
+    /// ====> Int, Float
+true false //booleans
+null undefined /// no value to them for variables
+
+NaN /// Not a number
+
+{} // Object nouns
+[] // arrays lists
+// Jared
+{
+    height: '5'11',
+    weight: '180',
+    eyeColor: 'Blue',
+    age: 35,
+    birthday: 'November',
+    isMale: true,
+    isFemale: false,
+    livesInUSA: true,
+    address:{
+        street: 'Hi',
+        city: 'Herriman',
+            state: 'Utah',
+            zipCode: '11111'
+    }
+}
+
+myFavoriteFoods: ['Pizza', 'Steak', 'Salad'],
+myFamilyMembers: [
+    {
+        name:'Ashlee',
+        age: 31,
+        relationship:'Spouse'
+    },
+    {
+        name:'Coleson',
+        age: 8,
+        relationship:'Son'
+    }
+]
