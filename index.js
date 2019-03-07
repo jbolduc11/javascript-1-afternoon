9ce2bdd9cde1c978eb03bc1083c4891bfee36988
Jared <jaredbolduc@yahoo.com>
2019-03-06 19:54:18 -0700

    updates

--- index.js
+++ index.js
@@ -1,102 +1,199 @@
-//Nouns adjective and verbs
-// basic data types
-//'abc' "abc" 'abc' // strings
-//10 //numbers
-    /// ====> Int, Float
-//true false //booleans
-//null undefined /// no value to them for variables
+// // Nouns adjective and verbs.
 
-//NaN /// Not a number
+// ///Basic Data Types
+// 'abc' "abc" `abc` ///strings
+// 10 //numbers
+//     /// ====> Int, Float 
+// true false //booleans
 
-//{} // Object nouns
-// [] // arrays lists
-// Jared
-const jared = {
-    height: 511,
+// null undefined /// Valueless 
+
+// NaN /// Not A Number. 
+
+// {} //Object nouns
+// [] //arrays lists 
+
+// Josh
+const josh = {
+    height: `5'11`,
     weight: '180',
-    eyeColor: 'Blue',
-    age: 35,
-    birthday: 'November',
+    eyeColor: 'blue',
+    age: 26,
+    birthday:'11/05/1992',
+    email:'veryjoshua@gmail.com',
     isMale: true,
     isFemale: false,
     livesInUSA: true,
     address:{
-        street: 'Hi',
-        city: 'Herriman',
-            state: 'Utah',
-            zipCode: '11111'
+        street: '6020 roseate spoonbill Drive',
+        city: 'Windermere',
+          state: 'Florida',
+          ZipCode: '34786'
     },
-myFavoriteFoods: ['Pizza' ,`Steak` ,`Salad`],
-myFamilyMembers=[
-    {
-        name:'Ashlee',
-        age: 31,
-        relationship:'Spouse'
-    },
-    {
-        name:'Coleson',
-        age: 8,
-        relationship:'Son'
-    }
-]
+    myFavoriteFoods:[`Pizza`, `Steak`, `Tamales`, `pozole`],
+    myFamilyMembers:[
+        {
+            name:'Lauren',
+            age: 22,
+            relationship:'Spouse'
+        },
+        {
+          name:'Eli',
+          age: 1,
+          relationship:'Child'
+      },
+    ],
+    addPostive: function(num1, num2) {
+          if(num1 > num2){
+              return num1 - num2;
+          }else{
+              return num2 - num1
+          }
+      }, 
+  }
+  
+  // > greater than
+  // < less than
+  // === equal to 
+  // !== not equal to
+  // && both condition have to be true
+  // || either or have to be true. 
+  
+  
+  
+  
+  
+  console.log(josh.addPostive(1,2))
+  // const name = 'josh';
+  // const isTrue = false;
+  // const age = 26
+  // const arr = [
+  //     [1,2,3],
+  //     [4,5,6],
+  //     [7,8,9]
+  // ];
+  // const dan = {
+  //     isAmazingProgrammer: true,
+  // }
+  
+  // const inSideArr = arr[2]
+  
+  // console.log(inSideArr[1])
+  
+  // /// variables 
+  // var //es5
+  // let // es6 let me change what it points to. 
+  // const //es6 constant 
 
+// shift alt copy multiple lines
 
-/// variables
-// var /// es5
-// let // es6 can change what it points to
-// const // es6 can never change the address
+Falsy
+const stuff = null;
+const stuff1 = undefined;
+const stuff2 = 0;
+const stuff3 = false;
+const stuff4 = ``;
+const stuff5 = NaN;
 
-//const myFunc = function() {
-//}; //es5
+all other statements are truthy
 
-//const myOtherFunc = () => {
+// command d highlights all the same word
 
-//}; //es6
+const name = 'josh'
+switch(age + 10){
+    case josh :
+        console.log("Hello josh");
+        break;
+    case 'Dan'
+        console.log("Welcome Dan");
+        break;
+    default:
+        console.log("We don't know you");
+        break;
+}
 
-const myFunc = function() {
-    const differenceInAge = jared.age - myFamilyMembers[0].age;
-};
-console.log(myFunc());
-console.log(myFamilyMembers[0].age)
+const name = 'joshua'
 
+if(name === 'josh'){
+    console.log("Hello josh");
+}else if(name === 'dan'){
+    console.log("Welcome Dan");
+}else{
+    console.log("We don't know you");
+}
 
-const add = function(num1, num2) {
-    return num1 + num2 + 10
-}; //es5
+switch(name){
+    case 'josh' : 
+        console.log("Hello josh");
+        break;
+    case 'Dan' :
+        console.log("Welcome Dan");
+        break;  
+    default: 
+        console.log("We don't know you");
+        break;
+}
 
-const myVar = add(1,2);
+// ternary statements
 
-console.log(myVar)
+//Ternary
 
-// function expression   const variable = function() {
-
-//}
-
-//function declaration function myFunc(){
-
-//}
-
-// arrow function  const arrow = () => {}
-
-const arrow = () => {
-    var myVariable = ''
-    function innerFunction() {
-        apple = 3
+const ageIsGreaterThanOne = (age) => {
+    if(age > 0){
+        return true;
+    }else{
+        return false;
     }
 }
 
-const addPositive = function(num1, num2) {
-    if(num1 > num2){
-        return num1 - num2;
-    }else{
-        return num2 - num1
-    }
-}; //es5
+const ageIsGreaterThanOne2 = (age) => {
+    return age > 0 ? true : false;
+}
 
-// > greater
-// && both have to be tru
-// || either or have to be true
-// === equal to
-// !== not equal to
+console.log(ageIsGreaterThanOne2(-1))
 
-// function lives in an object it's called a method
+// dynamic data
+const josh = {
+    height: `5'11`,
+    weight: '180',
+    eyeColor: 'blue',
+    age: 26,
+    birthday:'11/05/1992',
+    email:'veryjoshua@gmail.com',
+    isMale: true,
+    isFemale: false,
+    livesInUSA: true,
+    address:{
+        street: '6020 roseate spoonbill Drive',
+        city: 'Windermere',
+          state: 'Florida',
+          ZipCode: '34786'
+    },
+    myFavoriteFoods:[`Pizza`, `Steak`, `Tamales`, `pozole`],
+    myFamilyMembers:[
+        {
+            name:'Lauren',
+            age: 22,
+            relationship:'Spouse'
+        },
+        {
+          name:'Eli',
+          age: 1,
+          relationship:'Child'
+      },
+    ],
+    addPostive: function(num1, num2) {
+          if(num1 > num2){
+              return num1 - num2;
+          }else{
+              return num2 - num1
+          }
+      }, 
+  }
+  
+  const num = 0
+  const name = `myFamilyMembers`
+  
+  console.log(josh.myFavoriteFoods)
+  console.log(josh.myFavoriteFoods[num])
+  console.log(josh[name])
