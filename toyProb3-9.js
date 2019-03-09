ba10030854a0686951332a7ab45aed86dd2c0426
Jared <jaredbolduc@yahoo.com>
2019-03-09 09:26:33 -0700

    toy problem

---
+++ toyProb3-9.js
@@ -0,0 +1,9 @@
+function firstReverseTry(arr) {
+    if (arr.length > 1) {
+      var first = arr[0];
+      arr[0] = arr[arr.length-1];
+      arr[arr.length-1] = first;
+    }
+    return arr
+  }
+  console.log(firstReverseTry([1,2,3,4,5]))
