dd3869e7ebbd119a351fe9c4f15d6f31645b4305
Jared <jaredbolduc@yahoo.com>
2019-03-09 19:20:10 -0700

    practice problems

--- tester.js
+++ tester.js
@@ -7,3 +7,20 @@ function greetUser(name) {
 }
 
 console.log(getUserInfo(`Bo`, `Peep`,greetUser))
+
+
+let num1 = '12' 
+console.log(Number(num1));
+
+
+function addTen( arr ) {
+    var newArr = [];
+   
+for( var i = 0; i < arr.length; i++ ) {
+    newArr.push(Number(arr[i]) + 10);
+  }
+  
+  return newArr;
+}
+
+console.log(addTen([10, '20', 30]));
