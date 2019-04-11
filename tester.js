52260f57e6b7d8ac09767bec76e67df8d3857fe7
Jared <jaredbolduc@yahoo.com>
2019-04-10 18:48:51 -0600

    update

--- tester.js
+++ tester.js
@@ -24,3 +24,45 @@ for( var i = 0; i < arr.length; i++ ) {
 }
 
 console.log(addTen([10, '20', 30]));
+
+
+var countSheep = function (num){
+    var sheep = '';
+    for( var i=0; i<num; i++) {
+        sheep += (i+1) + ' sheep...'
+    }
+    return sheep
+  }
+
+  console.log(countSheep(3));
+
+  function getPlanetName(id){
+    var name;
+    switch(id){
+      case 1:
+        name = 'Mercury'
+        break;
+      case 2:
+        name = 'Venus'
+        break;
+      case 3:
+        name = 'Earth'
+        break;
+      case 4:
+        name = 'Mars'
+        break;
+      case 5:
+        name = 'Jupiter'
+        break;
+      case 6:
+        name = 'Saturn'
+        break;
+      case 7:
+        name = 'Uranus'
+        break;
+      case 8:
+        name = 'Neptune'
+        break;
+    }
+
+    console.log(getPlanetName(2), 'Venus');
